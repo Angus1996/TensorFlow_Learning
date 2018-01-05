03ab2364f20f03477297c1b6cd426fc41ab9953d
angus <884816034@qq.com>
2018-01-05 15:26:45 +0800

    Signed-off-by: angus <884816034@qq.com>

--- Chapter3/session.py
+++ Chapter3/session.py
@@ -5,10 +5,18 @@ a = tf.constant([1.0, 2.0], name="a")
 b = tf.constant([2.0, 3.0], dtype=None, shape=None, name="b", verify_shape=False)
 result = tf.add(a, b, name="add")
 
-# 创建一个会话
-sess = tf.Session()
-# 使用这个创建好的会话来得到关心的运算的结果。比如可以调用 seee.run(result),
-# 来得到3.1节样例中张量result 的取值
-sess.run(result)
-# 关闭会话使得本次运行中使用到的资源可以被释放
-sess.close()
+# # 创建一个会话
+# sess = tf.Session()
+# # 使用这个创建好的会话来得到关心的运算的结果。比如可以调用 seee.run(result),
+# # 来得到3.1节样例中张量result 的取值
+# sess.run(result)
+# # 关闭会话使得本次运行中使用到的资源可以被释放
+# sess.close()
+
+# 创建一个会话，并通过Python 中的上下文管理器来管理这个会话
+with tf.Session() as sess:
+	print(sess.run(result))
+	#效果同上
+	print(result.eval(session=sess))
+# 不需要再调用 “Session.close()” 函数来关闭会话
+# 当上下文退出时会话关闭和资源释放也自动完成了
---
+++ Chapter6/LeNet5_eval.py
@@ -0,0 +1,48 @@
+import time
+import tensorflow as tf
+from tensorflow.examples.tutorials.mnist import input_data
+import LeNet5_infernece
+import LeNet5_train
+
+BATCH_SIZE = 100
+# 加载的时间间隔。
+EVAL_INTERVAL_SECS = 10
+
+def evaluate(mnist):
+    with tf.Graph().as_default() as g:
+        x = tf.placeholder(tf.float32, [
+            BATCH_SIZE,
+            LeNet5_infernece.IMAGE_SIZE,
+            LeNet5_infernece.IMAGE_SIZE,
+            LeNet5_infernece.NUM_CHANNELS],
+        name='x-input')
+        y_ = tf.placeholder(tf.float32, [None, LeNet5_infernece.OUTPUT_NODE], name='y-input')
+        validate_feed = {x: mnist.validation.images, y_: mnist.validation.labels}
+
+        y = LeNet5_infernece.inference(x, False, None)
+        correct_prediction = tf.equal(tf.argmax(y, 1), tf.argmax(y_, 1))
+        accuracy = tf.reduce_mean(tf.cast(correct_prediction, tf.float32))
+
+        variable_averages = tf.train.ExponentialMovingAverage(LeNet5_train.MOVING_AVERAGE_DECAY)
+        variables_to_restore = variable_averages.variables_to_restore()
+        saver = tf.train.Saver(variables_to_restore)
+
+        while True:
+            with tf.Session() as sess:
+                ckpt = tf.train.get_checkpoint_state(LeNet5_train.MODEL_SAVE_PATH)
+                if ckpt and ckpt.model_checkpoint_path:
+                    saver.restore(sess, ckpt.model_checkpoint_path)
+                    global_step = ckpt.model_checkpoint_path.split('/')[-1].split('-')[-1]
+                    accuracy_score = sess.run(accuracy, feed_dict=validate_feed)
+                    print("After %s training step(s), validation accuracy = %g" % (global_step, accuracy_score))
+                else:
+                    print('No checkpoint file found')
+                    return
+            time.sleep(EVAL_INTERVAL_SECS)
+
+def main(argv=None):
+    mnist = input_data.read_data_sets("MNIST_data", one_hot=True)
+    evaluate(mnist)
+
+if __name__ == '__main__':
+    main()
